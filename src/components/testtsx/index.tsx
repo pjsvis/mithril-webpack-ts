853c6545ebdaeb457612cee481e1736c3f5aab12
Peter John Smith <pjstarifa@gmail.com>
2020-07-30 20:32:29 +0100

    Try out various ways of creating and composing components

--- src/components/testtsx/index.tsx
+++ src/components/testtsx/index.tsx
@@ -23,6 +23,34 @@ class Component extends MithrilTsxComponent {
   }
 }
 
+const Description = {
+  view(vnode: m.CVnode) {
+    return (
+      <div>
+        This page demonstrates different ways to create and compose Mithril components.
+        <h3>TODO</h3>
+        <div class="pl4">
+          <ul>
+            <li>Pass props to components</li>
+            <li>
+              Figure out best way to define components: class/object/function
+            </li>
+            <li>
+              Looks like objects are the cleanest then wrap everything up in a
+              view export
+            </li>
+            <li>Do we need to use default exports?</li>
+            <li>
+              Why not use named exports like we did as best practice in React?
+            </li>
+			<li>Set up a CodeSandbox template for a Mithril starter project with tsx (try and import this project!!!)</li>
+          </ul>
+        </div>
+      </div>
+    );
+  },
+};
+
 const Obj = {
   view(vnode: m.CVnode) {
     return (
@@ -45,8 +73,18 @@ const Funct = () => {
   };
 };
 
+const page = {
+  view(vnode: m.CVnode) {
+    return (
+      <div class="w-50 mt2 br3 pa3 ba b--black-10 shadow-5">
+        {[m(Description), m(Component), m(Obj), m(Funct)]}
+      </div>
+    );
+  },
+};
+
 export default {
-  view(vnode) {
-    return m(".page.testtsx", [m(nav), m(Component), m(Obj), m(Funct)]);
+  view(vnode: m.CVnode) {
+    return m(".page.testtsx", [m(nav), m(page)]);
   },
 } as m.Component<{}, {}>;
